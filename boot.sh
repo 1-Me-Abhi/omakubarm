1e7c8d4a204ac4878920a562422032b27413f331
1-Me-Abhi <sudo.apt.abhi@gmail.com>
2025-12-20 11:55:04 +0530

    Update boot.sh for ARM64 edition with new repo URL

--- boot.sh
+++ boot.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-ascii_art='________                  __        ___.
-\_____  \   _____ _____  |  | ____ _\_ |__
- /   |   \ /     \\__   \ |  |/ /  |  \ __ \
-/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
-\_______  /__|_|  (____  /__|_ \____/|___  /
-        \/      \/     \/     \/         \/
+ascii_art='
+  ___  __  __    _    _  ___   _ ____       _    ____  __  __ 
+ / _ \|  \/  |  / \  | |/ / | | | __ )     / \  |  _ \|  \/  |
+| | | | |\/| | / _ \ | ' /| | | |  _ \    / _ \ | |_) | |\/| |
+| |_| | |  | |/ ___ \| . \| |_| | |_) |  / ___ \|  _ <| |  | |
+ \___/|_|  |_/_/   \_\_|\_\\___/|____/  /_/   \_\_| \_\_|  |_|
 '
 
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
+echo "=> Omakub ARM64 Edition - For Ubuntu 24.04+ on ARM64/x86_64"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Omakub..."
+echo "Cloning Omakub ARM64..."
 rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
+git clone https://github.com/1-Me-Abhi/omakubarm.git ~/.local/share/omakub >/dev/null
+if [[ -n "$OMAKUB_REF" ]]; then
 	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+	git fetch origin "${OMAKUB_REF}" && git checkout "${OMAKUB_REF}"
 	cd -
 fi
 
